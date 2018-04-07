ccc780c1f219f7e266ba54af221b9bcc07ca6a08
Arivu <Arivu@Arivu-PC>
2018-04-07 10:31:08 +0530

    new order

--- src/myfiles/Twodimension.java
+++ src/myfiles/Twodimension.java
@@ -1,10 +1,11 @@
 package myfiles;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Twodimension {
 	public static void main (String [] args)
 	{
-		int s;int row=0,col=0;
+		int s;int row=0,col=0;String temp;
 		System.out.println("enter number of names and location you want to enter");
 		Scanner n=new Scanner(System.in);
 		s=n.nextInt();
@@ -24,5 +25,15 @@ public class Twodimension {
 	}
 
 }
+	for (int a=0;a<row;a++)
+	{
+		for (int b=0;b<col;b++)
+		{
+			
+		System.out.println(data[a][b]);
+		}
+		
+	}
+	
 	}
 }
