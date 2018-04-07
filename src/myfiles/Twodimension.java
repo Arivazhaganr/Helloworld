ad9fc690c6256fea5a7d263ac32b3b6c06c16e57
Arivu <Arivu@Arivu-PC>
2018-04-07 09:28:04 +0530

    new two dimensio

---
+++ src/myfiles/Twodimension.java
@@ -0,0 +1,28 @@
+package myfiles;
+import java.util.Scanner;
+
+public class Twodimension {
+	public static void main (String [] args)
+	{
+		int s;int row=0,col=0;
+		System.out.println("enter number of names and location you want to enter");
+		Scanner n=new Scanner(System.in);
+		s=n.nextInt();
+	row=s;
+	col=s;
+	Scanner scanner = new Scanner (System.in); 
+
+	String [][]data=new String [row][col];
+	for (int i = 0; i < row; i++) {
+
+		for (int j = 0; j < col; j++) {
+
+		System.out.println("Enter the number for row " + i + " and column " + j + " : ");
+
+		data [i][j] = scanner.nextLine();
+		
+	}
+
+}
+	}
+}
